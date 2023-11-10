3fefe0d0e72ae4f0a536f2845fbe3985d2dde497
Joey Guziejka <joeyguziejka@live.com>
2023-11-09 23:10:38 -0800

    adds logic

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+// Trifolder D&D Character Display App
+// by Joey Guziejka
+// 11/9/2023
 let ddbChars = [];
+const TEMPLATE_OK = "content" in document.createElement("template")
 const apiUrl = 'https://character-service.dndbeyond.com/character/v5/character/';
 const testId = 111494816;
 
@@ -25,9 +29,72 @@ async function getDataFromDdbUrl() {
 }
 
 async function loadCharacterInfo() {
-	
+	for (let i = 0; i < ddbChars.length; i++) {
+		const ddbChar = ddbChars[i];
+		if (ddbChar && ddbChar.success && ddbChar.data) {
+			buildCharacterView(ddbChar.data);
+		} else {
+			console.log(ddbChar);
+			console.warn('fail');
+		}
+	}
 }
 
+function buildCharacterView(c) {
+	if (!TEMPLATE_OK) {
+		alert('browser no support templates!!!');
+		return;
+	}
+	const template = document.querySelector('#dc-template');
+	const clone = template.content.cloneNode(true);
+
+	let dc_img = clone.querySelector('.dc-img');
+	dc_img.setAttribute('src', c.decorations.avatarUrl);
+
+	let dc_name = clone.querySelector('.dc-name');
+	dc_name.textContent = c.name;
+
+	let dc_rccl = clone.querySelector('.dc-raceclass');
+	const className = c.classes.map(o => o.definition.name + ' ' + o.subclassDefinition.name).join(', ');
+	dc_rccl.innerHTML = `Race: ${c.race.fullName}<br />Class: ${className}`;
+
+	let stats = [];
+	// base equipped inv items
+	const eq_acs = c.inventory.filter(x => x.equipped).filter(y => y.definition.armorClass != null);
+	const baseAcVal = eq_acs.map(x => x.definition.armorClass).reduce((a, b) => a + b, 0);
+	if (!baseAcVal || isNaN(baseAcVal) || baseAcVal < 10) {
+		baseAcVal = 10;
+	}
+
+	const dex = c.stats.find(x => x.id === 2).value;
+	const DEX_SCORE = 'Dexterity Score';
+	// race stat choices
+	const dex_defs = c.choices.choiceDefinitions.filter(x => x.options.some(y => y.label === DEX_SCORE));
+	const set_id = dex_defs[0].id ?? '1960452172-2';
+	const dex_ids = dex_defs[0].options.filter(x => x.label === DEX_SCORE).map(y => y.id);
+	const racials =  c.choices.race.filter(x => x.id === set_id);
+	//let foo = racials.filter(x => x.label );
+	//racials[0].optionValue
+	//(x => x.type === 2 && x.subType === 5)
+
+
+	const ac = baseAcVal;
+	//16; // tbd
+	//inventory[0].equipped
+	//definition.armorClass
+	stats.push(`AC: ${ac}`);
+
+
+	let dc_statlist = clone.querySelector('.dc-stat-list');
+	for (let i = 0; i < stats.length; i++) {
+		let stat = document.createElement('li');
+		stat.textContent = stats[i];
+		dc_statlist.appendChild(stat);
+	}
+
+	const o_container = document.querySelector('#output');
+	o_container.appendChild(clone);
+}
 
 
 document.addEventListener("DOMContentLoaded", function(event) {
